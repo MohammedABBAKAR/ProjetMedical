cf1151a4b5d53a9813b4a089ccfef61defaf66ca
Mohammed ABBAKAR <73604525+MohammedABBAKAR@users.noreply.github.com>
2023-09-16 18:14:52 +0200

    bar de recharche accueil

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -1,6 +1,12 @@
 import "./Main.css";
-
+import { useState } from "react";
 const Main = () => {
+  const data = ["Médecine généraliste", "Dermatologie", "Anesthésiologie", "Médecine du sport", "Neurologie"];
+  const [value, setValue] = useState("");
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
   return (
     <div className="box">
        
@@ -8,11 +14,25 @@ const Main = () => {
         <div className="mains">
           <h1>Rendez vous avec vos Professionnels </h1>
           <form  className="form-main">
-            <input type="text" name="search" placeholder="Search..." className="FormeMain1" />
-            <input type="submit" name="" value="Search" className="btn-main"  />
+            <input type="text" name="search"  value={value} onChange={handleChange} placeholder="Search..." className="FormeMain1" />
+            <input type="submit" name="" value="Search" className="btn-main" onClick={() => console.log(value)} />
           </form>
+          <ul>
+        {value &&
+          data
+            .filter((element) =>
+              element.toLowerCase().includes(value.toLowerCase())
+            )
+            .map((element, index) => (
+              <li onClick={() => setValue(element)} key={index}>
+                {element}
+              </li>
+            ))}
+      </ul>
         </div>
+   
       </div>
+ 
     </div>
   );
 };
